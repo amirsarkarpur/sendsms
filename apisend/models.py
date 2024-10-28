1df76defe5a389becde967589a501ed9e8b412a5
Amirsarkarpour <amir.asa.nasa@gmail.com>
2024-10-28 22:43:23 +0330

    fixxxx

--- apisend/models.py
+++ apisend/models.py
@@ -8,6 +8,7 @@ class give_data (models.Model):
     last_name = models.CharField(blank=False,null=False,max_length=80)
     phone_number = models.CharField(
         max_length=11,
+        unique=False,
         validators=[
             RegexValidator(r'^09\d{9}$', 'شماره موبایل باید با 09 شروع شود و 11 رقم باشد.')
         ]
